5bef4ec0c8a11afa98ff75c8113e1781361294a5
lgilbert <lgilbert@ITLC3ML593.jhbdvt.co.za>
2021-04-16 10:24:11 +0200

    Added Tables and mongo db demo files

---
+++ MyfirstMavenProjectIT/src/test/java/com/org/MyFristMavenProjectIT/DemoReadFromMongodb.java
@@ -0,0 +1,27 @@
+package com.org.MyFristMavenProjectIT;
+
+
+
+
+//public class DemoReadTable {
+//
+//	//public static void main(String[] args) throws IOException {
+//		// Look at rows
+////				WebElement TogetRows = driver.findElement(By.xpath(tablexpath));
+////				List<WebElement>TotalRowsList = TogetRows.findElements(By.tagName("tr"));
+////				System.out.println("Total number of Rows in the table are : "+ TotalRowsList.size());
+////				
+////				// Look at columns
+////				WebElement ToGetColumns = driver.findElement(By.xpath("//tbody/tr[3]"));
+////				List<WebElement> TotalColsList = ToGetColumns.findElements(By.tagName("td"));
+////				System.out.println("Total Number of colums in row 1: "+TotalColsList.size());
+////				
+////				// Look at field values
+////				WebElement ToGetFieldValues = driver.findElement(By.xpath("//tbody/tr[1]/td[1]"));
+////				System.out.println("Field value for row 1 column 1: "+ ToGetFieldValues.getText());
+//		    
+//		}
+//
+//	}
+
+
