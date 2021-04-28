727e6a84b29d31b1706143ad218e5b1c78895c9c
eonkruger <eonkruger@gmail.com>
2021-04-28 17:51:53 +0200

    bank projects main and keywords

---
+++ MyfirstMavenProjectIT/src/test/java/BankProjectGenerateData/BankProjectGenerateDataKeywords.java
@@ -0,0 +1,65 @@
+package BankProjectGenerateData;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+
+import frameworkclasses.SeleniumFunctions;
+
+public class BankProjectGenerateDataKeywords {
+
+	//INSTANTIATE NEW INSTANCE OF SELENIUM FUNCTIONS
+	SeleniumFunctions sfSelenium = new SeleniumFunctions();
+	
+	//DRIVER VARIABLE
+	WebDriver driver;
+	// Set URL
+	
+	String pURL = "http://demo.guru99.com";
+	
+	//NAVIGATE TO URL
+	public void navigateToURL(String pURL) {
+		driver.get(pURL);
+		//sfSelenium.maximiseBrowserWindow();
+	}
+	
+	//NAVIGATE TO BANK PROJECT
+	// Click on Payment Gateway Link
+		public void clickBankProject() {
+			sfSelenium.clickLink("Bank Project");
+		}
+	
+	//GENERATE USERNAME AND PASSWORD
+		
+	//STORE USERNAME AND PASSWORD
+	
+	//NAVIGATE BACK TO URL
+	
+	//ENTER USERNAME AND PASSWORD GENERATED
+	//GENERATE REPORT
+	//TAKE SCREENSHOTS
+	
+	//IF STATEMENT FOR SUCCESS
+	//GENERATE REPORT
+	//TAKE SCREENSHOTS
+	//ASSERT ON IMAGE
+	//ADDITIONAL ASSERT ON IS DISPLAYED TEXT
+	
+	//IF STATEMENT FOR UNSUCCESSFUL LOGIN
+	//HANDLE THE ALERT POPUP
+	//GENERATE REPORT
+	//TAKE SCREENSHOTS
+	
+	//RUN TESTS
+		public void runTestReadFromFile () throws IOException, InterruptedException {
+			navigateToURL(pURL);
+		}
+		
+	//GENERATE REPORT
+	//TAKE SCREENSHOTS
+	//CLICK ON LOGOUT
+	
+	//PERFORM CLEANUP
+
+	//CLOSE SELENUM
+}
---
+++ MyfirstMavenProjectIT/src/test/java/BankProjectGenerateData/BankProjectGenerateDataMain.java
@@ -0,0 +1,13 @@
+package BankProjectGenerateData;
+
+public class BankProjectGenerateDataMain {
+
+	public static void main(String[] args) {
+		//CALL METHODS FROM KEYWORDS
+		
+		BankProjectGenerateDataKeywords bpGenerateInput = new BankProjectGenerateDataKeywords();
+		
+		//PERFORM CLEANUP
+	}
+
+}
