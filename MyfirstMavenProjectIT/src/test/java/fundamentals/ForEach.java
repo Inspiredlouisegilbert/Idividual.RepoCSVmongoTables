abd85072005792424b62bdddaf3daa36db6f53d7
Louise Gilbert <lgilbert@jhb.dvt.co.za>
2021-05-17 10:46:55 +0200

    adding the foreach file 	new file:   src/test/java/fundamentals/ForEach.java

---
+++ MyfirstMavenProjectIT/src/test/java/fundamentals/ForEach.java
@@ -0,0 +1,20 @@
+package fundamentals;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		//for (type variableName : arrayName) {
+		for (String i : cars) {
+		  System.out.println(i);
+		}
+		
+	}
+		
+	
+
+
+
+}
