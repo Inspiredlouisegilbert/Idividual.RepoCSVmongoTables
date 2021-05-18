841619145ddd18d09ca84fdc2923561d040576e7
Louise Gilbert <lgilbert@jhb.dvt.co.za>
2021-05-18 09:44:38 +0200

    modified:   src/test/java/fundamentals/ChildClass.java

--- MyfirstMavenProjectIT/src/test/java/fundamentals/ChildClass.java
+++ MyfirstMavenProjectIT/src/test/java/fundamentals/ChildClass.java
@@ -3,22 +3,22 @@ package fundamentals;
 import org.testng.annotations.Test;
 
 public class ChildClass extends ParentClass {
-	String name ="QAClickAcademy";
+	
 
 	
-	public void overRideExample() {
+	public void overLoadExample() {
 		System.out.println("overRideExample");
 	}
 	
-	public void overRideExample(String inputExample) {
+	public void overLoadExample(String inputExample) {
 		System.out.println("String Overrid Example:" + inputExample);
 	}
 	
-	public void overRideExample(int inputIntExample) {
+	public void overLoadExample(int inputIntExample) {
 		System.out.println("int Overrid Example:" +inputIntExample);
 	}
 	
-	public void overRideExample(int inputIntExample, String inputExample) {
+	public void overLoadExample(int inputIntExample, String inputExample) {
 		System.out.println("int Overrid Example:" +inputIntExample + " String Overrid Example:" + inputExample);
 	}
 	
@@ -27,10 +27,10 @@ public class ChildClass extends ParentClass {
 	{
 		getData();
 		System.out.println("I am in child class");
-		overRideExample();
-		overRideExample("passing in data");
-		overRideExample(1);
-		overRideExample(1,"passing in data");
+		overLoadExample();
+		overLoadExample("passing in data");
+		overLoadExample(1);
+		overLoadExample(1,"passing in data");
 	}
 
 }
